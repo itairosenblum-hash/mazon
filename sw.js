173151519c0996c768e0b08c4b78133062ec5ad2
itairosenblum-hash <itai.rosenblum@gmail.com>
2026-06-09 06:56:11 +0300

    SW: self-destruct + no caching

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-// SW v10 - self-destruct to clear all caches
+// SW: unregister and clear all caches
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.registration.unregister())
-      .then(() => self.clients.matchAll())
+      .then(() => self.clients.matchAll({ type: 'window' }))
       .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
